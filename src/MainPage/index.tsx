e70197b1a7db06490125f60fafde321a247bfba6
Amandaanderssn <tindraamanda@hotmail.se>
2025-10-21 20:22:18 +0200

    Added a fun animation from reactbits to my welcome text

--- src/MainPage/index.tsx
+++ src/MainPage/index.tsx
@@ -2,14 +2,35 @@ import React from "react"
 import SearchBtn from "../Common/Components/Search Button"
 import "./index.css"
 
+import SplitText from "../Common/Components/AnimatedWelcomeText/index";
+
 const MainPage = () => {
 
     const [hasSearched, sethasSearched] = React.useState(false)
 
+
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
+
     return (
         <div style={{ margin: 'auto' }} className="mainDiv">
             {!hasSearched &&
-                <h1>Check today’s weather and plan your day with a smile!!</h1>
+                // <h1>Check today’s weather and plan your day with a smile!!</h1>
+                <SplitText
+                    text="Check today’s weather and plan your day with a smile!!"
+                    className="text-2xl font-semibold text-center"
+                    delay={20}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
             }
             <SearchBtn setHasSearched={sethasSearched} />
         </div>
